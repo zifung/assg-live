b4e80946aa975e6d0b69b68fd933823cc57938e3
zifung <112604962+zifung@users.noreply.github.com>
2022-09-21 16:19:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c66mat85rnuv.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chewzifung-employee"
+customregion = "us-east-1"
