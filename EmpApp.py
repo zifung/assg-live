700b9b0e3d1a4c2d5d868b1a066df5f2796ee2d3
zifung <112604962+zifung@users.noreply.github.com>
2022-09-23 23:49:26 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -219,11 +219,8 @@ def FetchData():
 
         s3 = boto3.resource('s3')
         s3_object = s3.Bucket(custombucket).Object("emp-id-" + str(emp_id) + "_image_file").get()
-        Image_url = s3_object['Metadata'].read().decode()
-
-        image_url=Image_url
-
-        
+        image_url = s3_object
+ 
         if request.method == 'POST':
             id = empid
             fname = Fname
