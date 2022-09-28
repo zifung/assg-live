ffcf19167ff4a2625f01673c1d36fae0454bfdea
zifung <112604962+zifung@users.noreply.github.com>
2022-09-28 17:24:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "employee.c3iwpfovny7b.us-east-1.rds.amazonaws.com"
+customhost = "employee.clbcqbtylbe0.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "yeohchiheng-employee"
+custombucket = "hamsa-employee"
 customregion = "us-east-1"
